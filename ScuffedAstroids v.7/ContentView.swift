3c3eae01927a777364982249178a930f60cb0e4a
MikkoLaquindanum <mcl081500@gmail.com>
2022-07-06 17:43:11 -0400

    Initial Commit

---
+++ ScuffedAstroids v.7/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScuffedAstroids v.7
+//
+//  Created by Mikko Laquindanum on 7/6/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScuffedAstroids v.7/ScuffedAstroids_v_7App.swift
@@ -0,0 +1,17 @@
+//
+//  ScuffedAstroids_v_7App.swift
+//  ScuffedAstroids v.7
+//
+//  Created by Mikko Laquindanum on 7/6/22.
+//
+
+import SwiftUI
+
+@main
+struct ScuffedAstroids_v_7App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
